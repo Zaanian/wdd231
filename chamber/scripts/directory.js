91e50772d372292bc381125f60f710ba980071a7
Zaanian <sstauffer1@byupathway.edu>
2025-11-06 10:49:09 -0800

    y

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,8 @@
+
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//get the last modified date
+const lastmodifieddate = document.lastModified
+document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
