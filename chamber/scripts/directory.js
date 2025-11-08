2d0d8d047d2d3d7f96a157f20a98f5d229132ad7
Zaanian <sstauffer1@byupathway.edu>
2025-11-08 14:27:42 -0800

    tyu

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,3 +6,16 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 const lastmodifieddate = document.lastModified
 document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
 
+// hamburger button for menu in mobile size
+const hamButton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
+
+// json data retrival 
+
+
+
