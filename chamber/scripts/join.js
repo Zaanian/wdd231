75fd33e4f56a98c2c9d996c8b05fe99f326b0462
Zaanian <sstauffer1@byupathway.edu>
2025-11-24 09:53:27 -0800

    button learn more done

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -14,11 +14,54 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('show');
 });
 
-
-// modal 
+//modal
+const modalNP = document.getElementById('np-info');
+const modalBR = document.getElementById('bronze-info');
+const modalSI = document.getElementById('silver-info');
+const modalGO = document.getElementById('gold-info');
+//non-profit learn more
+const openNPBtn = document.getElementById('np-open');
+const closeNPBtn = document.getElementById('np-close');
+openNPBtn.addEventListener('click', () => {
+    modalNP.showModal();
+});
+closeNPBtn.addEventListener('click', () => {
+    modalNP.close();
+});
+//bronze learn more
+const openBRBtn = document.getElementById('br-open');
+const closeBRBtn = document.getElementById('br-close');
+openBRBtn.addEventListener('click', () => {
+    modalBR.showModal();
+});
+closeBRBtn.addEventListener('click', () => {
+    modalBR.close();
+});
+//silver learn more
+const openSIBtn = document.getElementById('si-open');
+const closeSIBtn = document.getElementById('si-close');
+openSIBtn.addEventListener('click', () => {
+    modalSI.showModal();
+});
+closeSIBtn.addEventListener('click', () => {
+    modalSI.close();
+});
+//gold learn more
+const openGOBtn = document.getElementById('go-open');
+const closeGOBtn = document.getElementById('go-close');
+openGOBtn.addEventListener('click', () => {
+    modalGO.showModal();
+});
+closeGOBtn.addEventListener('click', () => {
+    modalGO.close();
+});
+//
 
 
 
 
 
 //URLparam  GET
+
+
+
