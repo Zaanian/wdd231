122810538dfe39854c3730fd09740c6550f11f1b
Zaanian <sstauffer1@byupathway.edu>
2025-12-01 13:34:06 -0800

    yes

---
+++ chamber/data/interest.mjs
@@ -0,0 +1,53 @@
+export const places = {
+    "interest": [
+        {
+            "name": "Washington State Fair",
+            "address": "110 9th Ave SW, Puyallup, WA 98371",
+            "description": "A major event that takes place annually, but the grounds are also used for other events.",
+            "image": "images/puyallup-fair-size.webp"
+        },
+        {
+            "name": "Puyallups Farmer's Market",
+            "address": "330 S Meridian, Puyallup, WA 98371",
+            "description": "A popular local market known for its fresh produce and community atmosphere.",
+            "image": "images/puyallup-farmers-market-size.webp"
+        },
+        {
+            "name": "Point Defiance Park",
+            "address": "5400 N Pearl St, Tacoma, WA 98407",
+            "description": "A large urban park in nearby Tacoma that offers a zoo, aquarium, gardens, and scenic waterfront trails.",
+            "image": "images/point-defiance-park-size.webp"
+        },
+        {
+            "name": "LeMay - America's Car Museum",
+            "address": "2702 E D St, Tacoma, WA 98421",
+            "description": "A museum in Tacoma dedicated to the history and culture of the automobile.",
+            "image": "images/Car-Museum-size.webp"
+        },
+        {
+            "name": "Museum of Glass",
+            "address": "1801 Dock Street, Tacoma, WA 98402",
+            "description": "Also located in Tacoma, this museum features a wide variety of glass art, including a famous Chihuly Bridge of Glass connection.",
+            "image": "images/glass-museum-size.webp"
+        },
+        {
+            "name": "Point Defiance Zoo & Aquarium",
+            "address": "5400 North Pearl Street, Tacoma, WA 98407",
+            "description": "A comprehensive zoo and aquarium located within Point Defiance Park.",
+            "image": "images/PDZA-size.webp"
+        },
+        {
+            "name": "Mount Rainer National Park",
+            "address": "39000 State Route 706 E, Ashford, WA 98304",
+            "description": "A stunning natural area that is a few hours drive from Puyallup, offering hiking, scenic drives, and majestic mountain views.",
+            "image": "images/mount-rainer-size.webp"
+        },
+        {
+            "name": "Chihuly Garden and Glass",
+            "address": "305 Harrison St, Seattle, WA 98109",
+            "description": "Located in Seattle, this museum showcases the vibrant and intricate glass art of Dale Chihuly. ",
+            "image": "images/chihuly-size.webp"
+        }
+    ]
+}
+
---
+++ chamber/scripts/discovery.js
@@ -0,0 +1,55 @@
+
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//get the last modified date
+const lastmodifieddate = document.lastModified
+document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
+// hamburger button for menu in mobile size
+const hamButton = document.querySelector('#menu');
+const mainnav = document.querySelector('#navigation');
+
+hamButton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
+
+//
+import { places } from '../data/interest.mjs'
+console.log(places.interest)
+
+
+//
+const cards = document.getElementById("cards");
+const displayInterestGrid = (interests) => {
+    interests.forEach(interest => {
+        let card = document.createElement("section");
+        let name = document.createElement("h2")
+        let address = document.createElement("p")
+        let description = document.createElement("p")
+        let img = document.createElement("img")
+
+        card.classList.add("comp-cards")
+        name.textContent = `${interest.name}`
+        address.textContent = `${interest.address}`
+        description.textContent = `${interest.description}`
+
+        img.setAttribute("src", interest.image)
+        img.setAttribute('alt', `image of ${interest.name}`)
+        img.setAttribute('loading', 'lazy')
+        img.setAttribute('width', 'auto');
+        img.setAttribute('height', 'auto');
+
+        card.appendChild(name)
+        card.appendChild(address)
+        card.appendChild(description)
+        card.appendChild(img)
+
+        cards.appendChild(card)
+
+
+    });
+}
+
+displayInterestGrid(places.interest)
