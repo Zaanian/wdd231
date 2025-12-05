0a822f31ce63f9194e02a5b3385a4c897c451687
Zaanian <sstauffer1@byupathway.edu>
2025-12-05 11:11:16 -0800

    yes

---
+++ final/script/inventory.js
@@ -0,0 +1,45 @@
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//get the last modified date
+const lastmodifieddate = document.lastModified
+document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
+// hamburger button for menu in mobile size
+const hamButton = document.querySelector('#menu');
+const mainnav = document.querySelector('#navigation');
+
+hamButton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
+
+//
+
+import { miniatures } from "../data/minis-data.mjs";
+
+//
+const cards = document.getElementById("cards");
+const displayInventoryGrid = (minis) => {
+    minis.forEach(mini => {
+        let card = document.createElement("section");
+        let name = document.createElement("h2")
+        let game = document.createElement("p")
+
+
+        card.classList.add("comp-cards")
+        name.textContent = `${mini.name}`
+        game.textContent = `${mini.game}`
+
+
+
+        card.appendChild(name)
+        card.appendChild(game)
+
+        cards.appendChild(card)
+
+
+    });
+}
+
+displayInventoryGrid(miniatures.miniatures)
