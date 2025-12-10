96576ad5a8dd22b5dbbdf09ab130ca9346d4e5fd
Zaanian <sstauffer1@byupathway.edu>
2025-12-10 12:25:29 -0800

    yes

--- final/script/checkout.js
+++ final/script/checkout.js
@@ -1,24 +1,22 @@
 
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//get the last modified date
+const lastmodifieddate = document.lastModified
+document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
+// hamburger button for menu in mobile size
+const hamButton = document.querySelector('#menu');
+const mainnav = document.querySelector('#navigation');
+
+hamButton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
+
+
+
+
+
 //
-import { miniatures } from "../data/minis-data.mjs";
-
-const listData = miniatures.miniatures
-
-
-//
-function settCart() {
-    const shoppingCart = localStorage.setItem("mini", "name")
-}
-
-function checkCartUp() {
-    const shoppingCart = localStorage.getItem("mini")
-    if (shoppingCart) {
-        console.log("shop cart set.")
-
-    } else {
-        console.log("Shop cart not set. Please reload page.")
-        settCart()
-    }
-}
-checkCartUp()
-
